58936ad4f2f554f5deb2978f8235edc2d55cafd8
Rodrigo Felix <rodrigofelixferreira@hotmail.com>
2022-11-12 00:14:42 -0300

    Adicionando Numbers

---
+++ Numbers/src/main/kotlin/Main.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+    val num1 = 42.toFloat(); // transformando o tipo da variavel para float
+    val num2 = num1.toDouble(); // transformando o tipo da variavel para double
+
+
+    val distToMoon = 92_960_000;// Organizando com Underline
+
+    println(distToMoon)
+}
