fd77ca2751ed322635f30077892301a863dae70f
Rodrigo Felix <rodrigofelixferreira@hotmail.com>
2022-11-11 23:55:21 -0300

    Adicionando Hello World

---
+++ HelloWorld/src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>) {
+    var say = "Hello World! "; // mutable variable "var"
+    val say2 = "Nice to meet you!" // immutable variable "val"
+    println(say + say2)
+
+    // Usando String Templates.
+
+    val nome = "Rodrigo";
+    val frase = "Meu nome é $nome";
+
+    println(frase)
+
+}
