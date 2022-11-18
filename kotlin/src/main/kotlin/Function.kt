2a57217a2bff2bd57fa97ec1bc05e844f4a0ad5e
Rodrigo Felix <rodrigofelixferreira@hotmail.com>
2022-11-17 22:52:21 -0300

    Atualizando funcoes

--- kotlin/src/main/kotlin/Function.kt
+++ kotlin/src/main/kotlin/Function.kt
@@ -1,8 +1,11 @@
 // Function in a small part of program that can be reused
 fun main() {
-    println(sum(5, 5))
-    println(helloWorld())
-    test()
+    countChar("Rodrigo")
+    println(cube(3))
+    println(milesConverter(2f))
+    changeA("Amanda")
+    countChar("Rodrigo")
+
 }
 
 fun sum(a: Int, b: Int): Int {
@@ -17,5 +20,29 @@ fun helloWorld(): String {
 fun test() {
     println("Hello, Rodrigo")
 }
+
+fun yearsConvertor(years: Int): Unit  {
+    println("$years years equals")
+    println("${years * 12} months")
+    println("${years * 365} days")
+    println("${years * 365 * 24} hours")
+    println("${years * 365 * 24 * 60} minutes")
+    println("${years * 365 * 24 * 60 * 60} seconds")
+}
+
+fun countChar(String: String): Unit = println("$String tem ${String.length} Characters")
+
+
+fun cube(num: Int): Int = num * num * num
+
+fun milesConverter(m: Float): Float = (m * 1.6f)
+
+fun changeA(str: String) {
+    println(str.replace("a", "x", true).lowercase())
+}
+
+
+
+
 
 
---
+++ kotlin/src/main/kotlin/kotlinMath.kt
@@ -0,0 +1,6 @@
+import kotlin.math.*
+
+fun main() {
+    println(max(5, 10))
+    println(min(5, 5))
+}
