ff9473fff767f1476a0876eca7eb08feb9ba78b5
Rodrigo Felix <rodrigofelixferreira@hotmail.com>
2022-11-21 16:31:25 -0300

    upadate flux controll while for do while

---
+++ kotlin/src/main/kotlin/DoWhile.kt
@@ -0,0 +1,6 @@
+fun main() {
+   do {
+       println("What's your name? ")
+       var value: String = readln()
+   } while (value == "")
+}
---
+++ kotlin/src/main/kotlin/While.kt
@@ -0,0 +1,18 @@
+fun main() {
+
+    var i: Int =  0
+    while (i < 100) {
+        print("$i ")
+        i++
+    }
+
+    var j: Int = 100
+    while (j > 0) {
+        println(j)
+        j--
+    }
+
+
+
+
+}
