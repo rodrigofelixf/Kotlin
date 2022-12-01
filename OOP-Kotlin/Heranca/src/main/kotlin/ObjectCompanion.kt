65383f18bc1fdd25ffe4eec356396f7eff8de295
Rodrigo Felix <rodrigofelixferreira@hotmail.com>
2022-11-30 23:41:15 -0300

    Add ObjectCompanion

---
+++ OOP-Kotlin/Heranca/src/main/kotlin/ObjectCompanion.kt
@@ -0,0 +1,20 @@
+class Matematica {
+
+    companion object {
+        var teste: String? = null
+    }
+
+    object teste {
+        fun teste() {
+
+        }
+    }
+}
+
+fun main() {
+    val n = Matematica()
+    Matematica.teste.teste()
+
+
+
+}
