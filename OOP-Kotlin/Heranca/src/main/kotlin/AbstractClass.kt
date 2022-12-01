3bbd505b6de43e90d1a3715291f594faf9d72886
Rodrigo Felix <rodrigofelixferreira@hotmail.com>
2022-12-01 13:15:37 -0300

    Add AbstractClass

---
+++ OOP-Kotlin/Heranca/src/main/kotlin/AbstractClass.kt
@@ -0,0 +1,25 @@
+abstract class Mammal(val name: String) {
+
+
+    fun wakeUP() {}
+
+    fun sleep() {}
+
+    abstract fun talk()
+
+}
+
+class Dog(name: String) : Mammal(name) {
+    override fun talk() {
+        println("Au, AU!")
+    }
+
+}
+
+fun main() {
+
+    var dog1 = Dog("Querty")
+
+    dog1.talk()
+
+}
