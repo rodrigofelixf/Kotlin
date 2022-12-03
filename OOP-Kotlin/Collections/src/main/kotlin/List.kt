d6e4efd41402ad65cebc5293e8a9320c90f5ad70
Rodrigo Felix <rodrigofelixferreira@hotmail.com>
2022-12-02 21:42:24 -0300

    Add Set file

--- OOP-Kotlin/Collections/src/main/kotlin/List.kt
+++ OOP-Kotlin/Collections/src/main/kotlin/List.kt
@@ -1,15 +1,14 @@
 fun main() {
     // Immutable List
-    val list1: List<Int> = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    val list1: List<Int> = listOf(1, 2, 3, 4, 5, 3, 7, 8, 9, 8)
     // Muttable List
-    val list2: MutableList<Int> = mutableListOf()
+    val list2: MutableList<Int> = mutableListOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
 
 
-    print(list1.size)
-    list2.add(1)
 
 
-    for (l in list1) println(l)
+    println(list1.sorted())
+
 
 }
 
---
+++ OOP-Kotlin/Collections/src/main/kotlin/Set.kt
@@ -0,0 +1,10 @@
+fun main() {
+
+    val set1: Set<String> = setOf<String>()
+    val set2: MutableSet<String> = mutableSetOf<String>("Rodrigo", "Jessica", "Rosana", "Isabella", "Rodrigo")
+
+
+    println(set2.sorted())
+
+
+}
