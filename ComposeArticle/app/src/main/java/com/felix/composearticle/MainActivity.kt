12442fffa9b3666956938f2f975e2b4fdd2d5534
Rodrigo Felix <rodrigofelixferreira@hotmail.com>
2023-01-19 09:38:54 -0300

    Adicionado scroll no card do Article, e os textos do mesmo

--- ComposeArticle/app/src/main/java/com/felix/composearticle/MainActivity.kt
+++ ComposeArticle/app/src/main/java/com/felix/composearticle/MainActivity.kt
@@ -3,13 +3,23 @@ package com.felix.composearticle
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.felix.composearticle.ui.theme.ComposeArticleTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +32,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    ComposeArticleApp()
                 }
             }
         }
@@ -30,14 +40,60 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun ComposeArticleApp() {
+    ComposeCard(
+        title = stringResource(id = R.string.title_article),
+        shortDescription = stringResource(id = R.string.short_description),
+        longDescription = stringResource(id = R.string.long_description),
+        imagePainter = painterResource(id = R.drawable.bg_compose_background)
+    )
+}
+
+@Composable
+private fun ComposeCard(
+    title: String,
+    shortDescription: String,
+    longDescription: String,
+    imagePainter: Painter,
+    modifier: Modifier = Modifier,
+) {
+    val state = rememberScrollState()
+    LaunchedEffect(Unit) { state.animateScrollTo(100) }
+    Column(modifier = modifier.verticalScroll(state)) {
+        Image(
+            painter = imagePainter,
+            contentDescription = null
+        )
+        Text(
+            text = title,
+            fontSize = 24.sp,
+            modifier = Modifier
+                .padding(16.dp)
+        )
+        Text(
+            text = shortDescription,
+            fontSize = 24.sp,
+            modifier = Modifier
+                .padding(start = 16.dp, end = 16.dp),
+            textAlign = TextAlign.Justify
+        )
+        Text(
+            text = longDescription,
+            fontSize = 24.sp,
+            modifier = Modifier
+                .padding(16.dp),
+            textAlign = TextAlign.Justify
+        )
+
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun ComposeArticleAppPreview() {
     ComposeArticleTheme {
-        Greeting("Android")
+        Surface {
+            ComposeArticleApp()
+        }
     }
 }
