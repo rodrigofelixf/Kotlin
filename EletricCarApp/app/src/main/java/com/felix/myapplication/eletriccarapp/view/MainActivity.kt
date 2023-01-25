6c59dc421122deabec8945290af610bd7d63c3ce
Rodrigo Felix <rodrigofelixferreira@hotmail.com>
2023-01-25 10:20:11 -0300

    added activity main, input in Constraint layout

--- EletricCarApp/app/src/main/java/com/felix/myapplication/eletriccarapp/view/MainActivity.kt
+++ EletricCarApp/app/src/main/java/com/felix/myapplication/eletriccarapp/view/MainActivity.kt
@@ -3,6 +3,8 @@ package com.felix.myapplication.eletriccarapp.view
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.View.OnClickListener
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -13,8 +15,7 @@ import com.felix.myapplication.eletriccarapp.R
 import com.felix.myapplication.eletriccarapp.databinding.ActivityMainBinding
 import com.felix.myapplication.eletriccarapp.viewmodel.somaViewModel
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var viewModel: somaViewModel
+class MainActivity : AppCompatActivity(), OnClickListener {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,16 +23,17 @@ class MainActivity : AppCompatActivity() {
         supportActionBar?.hide()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.buttonCalculateMain.setOnClickListener() {
-            startActivity(Intent(this,CalculateActivity::class.java))
-        }
 
-        binding.imageCar01.setOnClickListener(){
+        //eventos de click
+        binding.buttonCalculateMain.setOnClickListener(this)
 
-        }
     }
 
-
+    override fun onClick(view: View) {
+        if (view.id == R.id.button_calculate_main) {
+            startActivity(Intent(this,CalculateActivity::class.java))
+        }
+    }
 
 
 }
