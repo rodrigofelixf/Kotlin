3a7e45beccd52e75f0ef0c5e244c9f0b10ae18b9
Rodrigo Felix <rodrigofelixferreira@hotmail.com>
2023-01-24 23:54:31 -0300

    added back inc, validation calculatem

--- EletricCarApp/app/src/main/java/com/felix/myapplication/eletriccarapp/view/CalculateActivity.kt
+++ EletricCarApp/app/src/main/java/com/felix/myapplication/eletriccarapp/view/CalculateActivity.kt
@@ -1,7 +1,9 @@
 package com.felix.myapplication.eletriccarapp.view
 
 import android.os.Bundle
+import android.view.View
 import android.view.View.OnClickListener
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -9,29 +11,47 @@ import com.felix.myapplication.eletriccarapp.R
 import com.felix.myapplication.eletriccarapp.databinding.ActivityCalculateBinding
 import com.felix.myapplication.eletriccarapp.viewmodel.somaViewModel
 
-class CalculateActivity : AppCompatActivity() {
+class CalculateActivity : AppCompatActivity(), OnClickListener {
     private lateinit var viewModel: somaViewModel
     private lateinit var binding: ActivityCalculateBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
         binding = ActivityCalculateBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        calculateHandle()
+
+        viewModel = ViewModelProvider(this).get(somaViewModel::class.java)
+
+        //eventos
+        binding.buttonCalculate.setOnClickListener(this)
+        binding.imageBack.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        if (view.id == R.id.button_calculate) {
+            calculateHandle()
+        } else if (view.id == R.id.image_back)
+            finish()
     }
 
     private fun calculateHandle() {
-        viewModel = ViewModelProvider(
-            this
-        ).get(somaViewModel::class.java)
-
-        binding.buttonCalculate.setOnClickListener {
+        if (isValid()) {
             viewModel.numb1.value = binding.editPriceKwh.text.toString().toInt()
             viewModel.numb2.value = binding.editKmRun.text.toString().toInt()
             viewModel.calcularSoma()
-        }
-        viewModel.resultado.observe(this, Observer {
-            binding.textCalculo.text = "Soma $it"
-        })
+
+            viewModel.resultado.observe(this, Observer {
+                binding.textCalculo.text = "Soma $it"
+            })
+        } else
+            Toast.makeText(this,"Preencha todos os campos", Toast.LENGTH_SHORT).show()
     }
+
+    private fun isValid(): Boolean {
+        return (
+                binding.editPriceKwh.text.toString() != "" && binding.editKmRun.text.toString() != "")
+    }
+
+
 }
